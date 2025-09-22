443f968cacf0e63896996952c8bb49b4dc1d060b
DatTD <dat.truong@vioapp.io>
2025-09-22 14:52:26 +0700

    feat(auth): enhance OTP verification process with phone support
    - Added phone number support for OTP verification in SendOTPSerializer.
    - Updated check_valid_verification utility to handle phone verification.
    - Modified OtpCode model to include 'to' and 'target' fields for better tracking.

--- core/user/admin.py
+++ core/user/admin.py
@@ -28,9 +28,8 @@ class GroupAdmin(BaseGroupAdmin, ModelAdmin):
 
 
 class OtpCodeAdmin(ModelAdmin):
-    list_display = ["user", "type_otp", "created_at"]
-    list_filter = ["type_otp"]
-    autocomplete_fields = ["user"]
+    list_display = ["to", "type_otp", "target", "created_at"]
+    list_filter = ["type_otp", "target"]
 
 
 class UserSettingsAdmin(ModelAdmin):
