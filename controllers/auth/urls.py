06d418a4ddf014ffacd61bbecb4aa2f03bef01d4
DatTD <dat.truong@vioapp.io>
2025-09-26 17:38:49 +0700

    feat(auth): remove unused token_verify endpoint from URL patterns

--- controllers/auth/urls.py
+++ controllers/auth/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import (
-    token_verify,
     token_blacklist,
     token_refresh,
     token_obtain_pair,
@@ -17,7 +16,6 @@ urlpatterns = [
     path("token/", token_obtain_pair, name="token_obtain_pair"),
     path("token/blacklist/", token_blacklist, name="token_blacklist"),
     path("token/refresh/", token_refresh, name="token_refresh"),
-    path("token/verify/", token_verify, name="token_verify"),
     path("register/", RegisterUserView.as_view(), name="register"),
     path("otp/send/", SendOTPView.as_view(), name="otp_send"),
     path("otp/verify/", VerifyOTPView.as_view(), name="otp_verify"),
